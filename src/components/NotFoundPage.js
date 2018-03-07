2063a37d5dccdd9f90d8d8754ec5d16b93d458f0
Christopher King <chris@cking.me>
2018-03-07 12:27:14 -0500

    begin styling the 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => {
   return (
     <div>
-      <h4>
-        404 Page Not Found
-      </h4>
-      <Link to="/"> Go back to homepage </Link>
+      <center>
+        <h1>
+          404 Page Not Found
+        </h1>
+        <br />
+        <p>Hey..um...I've been trying to find the words to tell you, but this just isn't working.</p>
+        <p>It's not you, it's me. I promise.</p>
+        <p>Actually, we just couldn't find the page you were looking for. Give us another chance, will you?</p>
+        <Link to="/"> Go back to homepage </Link>
+      </center>
     </div>
   );
 };
