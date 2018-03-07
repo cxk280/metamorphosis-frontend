40341646c51f8b043ca89cc8236075f122abee25
Christopher King <chris@cking.me>
2018-03-07 13:42:07 -0500

    finish styling chatLeft component

--- src/components/ChatLeft.js
+++ src/components/ChatLeft.js
@@ -6,6 +6,14 @@ class ChatLeft extends React.Component {
     return (
       <center>
         <div className="chatLeft">
+          <div className="imageParentChatLeft">
+            <img className="metamorphosisLogo" src="../images/Metamorphosis-Logo-Final.svg"/>
+          </div>
+          <div>
+            <form action="/logout" method="get" name="logoutForm">
+              <input className="chatLeftLogout" type="submit" value="Logout" />
+            </form>
+          </div>
         </div>
       </center>
     );
