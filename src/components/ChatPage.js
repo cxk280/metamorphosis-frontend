e0ce5296c617ff7e975ba9706ac3abf6c454a798
Christopher King <chris@cking.me>
2018-03-07 14:13:18 -0500

    style all background elements in proper proportions on chat page

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -4,12 +4,14 @@ import ChatMain   from './ChatMain';
 import ChatTop    from './ChatTop';
 import ChatBottom from './ChatBottom';
 import ChatLeft   from './ChatLeft';
+import ChatRight  from './ChatRight';
 
 class ChatPage extends React.Component {
   render() {
     return (
       <div className="chatPage">
-            <ChatLeft />
+          <ChatLeft />
+          <ChatRight />
           <ChatTop />
           <ChatMain />
           <ChatBottom />
---
+++ src/components/ChatRight.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class ChatRight extends React.Component {
+  render() {
+    return (
+      <center>
+        <div className="chatRight">
+          <h3>ChatLeft</h3>
+        </div>
+      </center>
+    );
+  };
+};
+
+export default ChatRight;
