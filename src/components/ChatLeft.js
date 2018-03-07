3c4c37b19db6ae73dc943d8ae86dccccc0777dce
Christopher King <chris@cking.me>
2018-03-07 13:31:17 -0500

    properly position chatLeft

---
+++ src/components/ChatLeft.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class ChatLeft extends React.Component {
+  render() {
+    return (
+      <center>
+        <div className="chatLeft">
+        </div>
+      </center>
+    );
+  };
+};
+
+export default ChatLeft;
--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ChatMain from './ChatMain';
-import ChatTop from './ChatTop';
+import React      from 'react';
+import { Link }   from 'react-router-dom';
+import ChatMain   from './ChatMain';
+import ChatTop    from './ChatTop';
 import ChatBottom from './ChatBottom';
+import ChatLeft   from './ChatLeft';
 
 class ChatPage extends React.Component {
   render() {
     return (
       <div className="chatPage">
+          <ChatLeft />
           <ChatTop />
           <ChatMain />
           <ChatBottom />
