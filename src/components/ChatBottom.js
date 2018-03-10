f6562c3a5b551feaf848cbc0349a51261ba43e35
Christopher King <chris@cking.me>
2018-03-10 12:13:11 -0500

    add ability to access state in fetch

--- src/components/ChatBottom.js
+++ src/components/ChatBottom.js
@@ -14,13 +14,36 @@ class ChatBottom extends React.Component {
   }
 
   componentDidUpdate(){
-    console.log('this.state.data: ',this.state.data);
+    console.log('this.state.data before fetch: ',this.state.data);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log('this.state.data in fetch: ',this.state.data);
+        console.log('response: ',response);
+        console.log('response.json(): ',response.json());
+    })
   }
 
   handleKeyPress(e) {
     if(e.key == "Enter"){
       if(e.preventDefault) e.preventDefault();
       this.setState({data: e.target.value})
+      // console.log('e.target.value before Ajax: ',e.target.value);
+      // $.ajax({
+      //   url: '/kafka',
+      //   type: 'POST',
+      //   dataType: 'json',
+      //   data: e.target.value,
+      //   success: function(data) {
+      //     console.log('Ajax success');
+      //     console.log('e.target.value in Ajax success: ',e.target.value);
+      //     // console.log('data: ',data);
+      //   },
+      //   error: function(data) {
+      //     console.log('There was an Ajax error');
+      //     // console.log('e.target.value in Ajax failure: ',e.target.value);
+      //     console.log('data: ',data);
+      //   }
+      // });
       e.target.value = '';
     };
   }
