fcdbe3227efc24c5ef60fa097a978c302ed0023a
Christopher King <chris@cking.me>
2018-03-12 19:10:58 -0400

    display current time on chat page

--- src/components/ChatItemLeft.js
+++ src/components/ChatItemLeft.js
@@ -1,6 +1,41 @@
 import React from 'react';
 
 class ChatItemLeft extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        time: Math.round(new Date().getTime()/1000.0)
+    };
+  }
+
+  componentWillMount(){
+    console.log('this.state.time: ',this.state.time);
+    //Convert UNIX time to date
+    let a = new Date(this.state.time*1000);
+    let year = a.getFullYear();
+    year = year.toString();
+    console.log('year: ',year);
+    let month = a.getMonth() + 1;
+    month = month.toString();
+    console.log('month: ',month);
+    if (month.length === 1) {
+      month = '0' + month;
+    }
+    let date = a.getDate();
+    date = date.toString();
+    console.log('month: ',month);
+    if (date.length === 1) {
+      date = '0' + date;
+    }
+    let formattedTime = month + '-' + date + '-' + year;
+
+    console.log('formattedTime: ',formattedTime);
+    this.state.time = formattedTime;
+    console.log('this.state.time after setting state: ',this.state.time);
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +46,7 @@ class ChatItemLeft extends React.Component {
             <p className="chatTextLeft">Chat message</p>
           </div>
           <div className="chatTimeLeft">
-            <p>Chat time</p>
+            <p>{this.state.time}</p>
           </div>
       </div>
     );
