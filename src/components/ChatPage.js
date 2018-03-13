543f23e44194500f7655752b84cd1b079eedbcc7
Christopher King <chris@cking.me>
2018-03-12 21:15:38 -0400

    move code from componentDidUpdate to handleKeyPress

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -10,7 +10,7 @@ class ChatPage extends React.Component {
     super(props);
 
     this.state = {
-        user: '',
+        user: 'Chris',
         message: ''
     };
 
@@ -38,35 +38,42 @@ class ChatPage extends React.Component {
     })
   }
 
-  componentDidUpdate(){
-    fetch("http://localhost:8082/topics/jsontest", {
-      body: "{\"records\":[{\"value\":{\"user\": \"" + this.state.user + "\", \"message\": \"" + this.state.message + "\"}}]}",
-      headers: {
-        "Content-Type": "application/vnd.kafka.json.v2+json"
-      },
-      method: "POST"
-    })
-    .then(response => {
-      fetch("http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/records", {
-        headers: {
-          Accept: "application/vnd.kafka.json.v2+json"
-        }
-      })
-      .then(response => response.json())
-        .then(data => {
-          if((this.state.user != data[0].value.user) || (this.state.message != data[0].value.message)) {
-            this.setState({ user: data[0].value.user, message: data[0].value.message});
-          } else {
-            return
-          }
-        })
-    })
-  }
-
   handleKeyPress(e) {
     if(e.key == "Enter"){
       if(e.preventDefault) e.preventDefault();
-      this.setState({user: 'Chris', message: e.target.value})
+
+      fetch("http://localhost:8082/topics/jsontest", {
+        body: "{\"records\":[{\"value\":{\"user\": \"" + this.state.user + "\", \"message\": \"" + e.target.value + "\"}}]}",
+        headers: {
+          "Content-Type": "application/vnd.kafka.json.v2+json"
+        },
+        method: "POST"
+      })
+      .then(response => {
+        console.log('first then');
+        fetch("http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/records", {
+          headers: {
+            Accept: "application/vnd.kafka.json.v2+json"
+          }
+        })
+        .then(response => response.json())
+          .then(data => {
+            console.log('last then');
+            console.log('data: ',data);
+            console.log('this.state.user: ',this.state.user);
+            console.log('data[0].value.user: ',data[0].value.user);
+            // console.log(this.state.message);
+            // console.log(data[0].value.message);
+            if((this.state.user != data[0].value.user) || (this.state.message != data[0].value.message)) {
+              this.setState({ user: data[0].value.user, message: data[0].value.message});
+              console.log('this.state.user after setState: ',this.state.user);
+              console.log('this.state.message after setState: ',this.state.message);
+            } else {
+              console.log('returning')
+              return
+            }
+          })
+      })
       e.target.value = '';
     }
   }
